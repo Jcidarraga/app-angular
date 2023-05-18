c6e6f6d1bcbfce69fb7363f8c84ce4a6066fd1a5
Jcidarraga <camilobedoya021114@gmail.com>
2023-05-17 22:10:02 -0500

    recuperar contraseña listo

--- src/app/components/forgot-pass/forgot-pass.component.ts
+++ src/app/components/forgot-pass/forgot-pass.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router, ActivatedRoute, UrlSegment } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { FirebaseCodeErrorService } from 'src/app/services/firebase-code-error.service';
 
 @Component({
   selector: 'app-forgot-pass',
@@ -8,14 +12,28 @@ import { Router, ActivatedRoute, UrlSegment } from '@angular/router';
 })
 export class ForgotPassComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  recuperarUser: FormGroup;
+  loading: boolean = false;
+
+  constructor(private router: Router, private fb: FormBuilder,
+    private afAuth: AngularFireAuth, private toastr: ToastrService,
+    private firebaseError: FirebaseCodeErrorService) {
+    this.recuperarUser = this.fb.group({
+      correo: ['', Validators.required],
+    })
+  }
+
   ngOnInit() {
   }
+  recuperar() {
+    const email = this.recuperarUser.value.correo
+    this.loading = true;
 
-  goLogin() {
-    //  this.isLoggedIn = true;
-    this.router.navigate(['/login']);
-    // localStorage.setItem('isLoggedIn', String(this.isLoggedIn));
+    this.afAuth.sendPasswordResetEmail(email).then(() => {
+      this.toastr.info('Le envie el correo mmhuevo', 'Recuperar contraseña')
+      this.router.navigate(['/login'])
+    }).catch((error) => {
+      this.toastr.error(this.firebaseError.codeError(error.code), 'Error')
+    })
   }
-
 }
